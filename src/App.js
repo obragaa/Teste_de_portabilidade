9710b83785ea20223128cd82cd5a2485dbe11ca3
Felipe Braga Duarte <60896979+obragaa@users.noreply.github.com>
2021-04-19 17:28:07 -0300

    Criando uma melhor responsividade

--- src/App.js
+++ src/App.js
@@ -40,46 +40,50 @@ function App() {
 
         </ul>
 
-          <div className="container-pai">
-            {champions.map(champion => ( 
-
-              <div className="container-filho">
-
-                <div className="conteudo">
-                  
-                  <div key={champion.id} id="header-heroi">
-                    <p id="texto-header" >{champion.name}/{champion.function}</p>
-                  </div>
-
-                  <div key={champion.id} id="img-heroi ">
-                    <img id="img-border" src={champion.img_champion} width="220px" heigth="220px"></img>
-                  </div>
-                  
-                  <div key={champion.id} className="infos-heroi">
-
-                    <img className="skill-img" src={champion.img_passive} width="50px" heigth="50px"></img>
-                    <p id="texto-infos">{champion.passive}</p>
-
-                    <img className="skill-img" src={champion.Q_img} width="50px" heigth="50px"></img>
-                    <p id="texto-infos"> Q: {champion.Q}</p>
-
-                    <img className="skill-img" src={champion.W_img} width="50px" heigth="50px"></img>
-                    <p id="texto-infos"> W: {champion.W}</p>
-
-                    <img className="skill-img" src={champion.E_img} width="50px" heigth="50px"></img>
-                    <p id="texto-infos"> E: {champion.E}</p>
-
-                    <img className="skill-img" src={champion.R_img} width="50px" heigth="50px"></img>
-                    <p id="texto-infos"> R: {champion.R}</p>
-
-                  </div>
-                  
+        <div className="container-pai">
+          {champions.map(champion => ( 
+            
+              <div className="conteudo">
+                <div key={champion.id} id="header-heroi">
+                  <p id="texto-header" >{champion.name}/{champion.function}</p>
                 </div>
 
-              </div>
+                <div key={champion.id} id="img-heroi ">
+                  <img id="img-border" src={champion.img_champion}></img>
+                </div>
+                
+                <div key={champion.id} className="infos-heroi">
 
-            ))}
-          </div>
+                  <div className="divEncapsulamento">
+                    <img className="skill-img" src={champion.img_passive}></img>
+                    <p id="texto-infos">{champion.passive}</p>
+                  </div>
+
+                  <div className="divEncapsulamento">
+                    <img className="skill-img" src={champion.Q_img}></img>
+                    <p id="texto-infos"> Q: {champion.Q}</p>
+                  </div>
+
+                  <div className="divEncapsulamento">
+                    <img className="skill-img" src={champion.W_img}></img>
+                    <p id="texto-infos"> W: {champion.W}</p>
+                  </div>
+
+                  <div className="divEncapsulamento">
+                    <img className="skill-img" src={champion.E_img}></img>
+                    <p id="texto-infos"> E: {champion.E}</p>
+                  </div>
+
+                  <div className="divEncapsulamento">
+                    <img className="skill-img" src={champion.R_img}></img>
+                    <p id="texto-infos"> R: {champion.R}</p>
+                  </div>
+
+                </div>
+              </div>
+            
+          ))}
+        </div>
 
         <footer className="bg-dark text-center text-lg-start footer">
 
